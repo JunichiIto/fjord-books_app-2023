65b0eb02ef672568fd88d22606c00a329161e94c
Junichi Ito <jit@sonicgarden.jp>
2023-01-02 11:06:27 +0900

    バリデーションエラー表示の国際化対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,11 @@ module ApplicationHelper
   def i18n_pluralize(word)
     I18n.locale == :ja ? word : word.pluralize
   end
+
+  # localeに応じてエラー件数の表記を変える
+  # - 日本語の場合 => 3件のエラー
+  # - 英語の場合 => 3 errors
+  def i18n_error_count(count)
+    I18n.locale == :ja ? "#{count}件の#{t('views.common.error')}" : pluralize(count, t('views.common.error'))
+  end
 end
