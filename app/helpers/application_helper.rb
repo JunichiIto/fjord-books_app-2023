c6bef7cbdf133fdd7cdbeff51791fa9d283ea604
Junichi Ito <jit@sonicgarden.jp>
2023-01-02 10:55:25 +0900

    国際化対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # localeに応じて複数形の表記を変える
+  # - 日本語の場合 => 本
+  # - 英語の場合 => books
+  def i18n_pluralize(word)
+    I18n.locale == :ja ? word : word.pluralize
+  end
 end
